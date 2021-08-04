b9b57b2517d0a0615c8aaa44f3f15a2ed800d564
A-Huan <88374213+yang-ahuan@users.noreply.github.com>
2021-08-04 16:41:19 +0800

    Upload

--- contains_duplicate.py
+++ contains_duplicate.py
@@ -1,4 +1,5 @@
 # Intuition : Leverage the property of set
+# Time/space complexity : O(1) / O(1)
 class Solution:
     def containsDuplicate(self, nums: list) -> bool:
         return len(nums) != len(set(nums))
