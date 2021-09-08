33f98ee01d5d24fba893e5109d504f512d130644
A-Huan <88374213+yang-ahuan@users.noreply.github.com>
2021-09-08 14:41:12 +0800

    Upload

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -16,7 +16,8 @@ class Solution:
             record = maximum
         return record
 
-# Idea : It's Kadane’s algorithm. The algorithm is that find local maximum at index i is the maximum of A[i] and "the sum of A[i] and previous maximum at index i-1"(main idea).
+# Idea : It's Kadane’s algorithm. The algorithm is that find local maximum at index i is the maximum of A[i]
+#        and "the sum of A[i] and previous maximum at index i-1"(main idea).
 # (from https://medium.com/@rsinghal757/kadanes-algorithm-dynamic-programming-how-and-why-does-it-work-3fd8849ed73d)
 # Time/space complexity : O(n)/O(1)
 class Solution:
@@ -30,8 +31,9 @@ class Solution:
                 maximum = record
         return maximum
 
-# Idea : It's divide and conquer. Divide array into two subarraies and find maximum summation of a subarray individually, l_max and r_max. Then, we calculate the maximum summation of subarray crossing left and right subarray, c_max.
-#        Finally, compare among l_max, r_max and c_max and get maximum summation.(林立宇演算法筆記)
+# Idea : It's divide and conquer. Divide array into two subarraies and find maximum summation of a subarray individually, l_max and r_max.
+#        Then, we calculate the maximum summation of subarray crossing left and right subarray, c_max. Finally, compare among l_max, r_max
+#        and c_max and get maximum summation.(林立宇演算法筆記)
 # Time/space complexity : O(nlogn)/O()
 class Solution:
     def maxSubArray(self, nums: list) -> int:
@@ -39,5 +41,6 @@ class Solution:
         if length == 1: return nums[0]
         l_max = self.maxSubArray(nums[:length//2])
         r_max = self.maxSubArray(nums[length//2:])
-        c_max = max([sum(nums[length//2:idx]) for idx in range(length//2+1, length+1)]) +                       max([sum(nums[idx:length//2]) for idx in range(length//2-1, -1, -1)])
+        c_max = max([sum(nums[length//2:idx]) for idx in range(length//2+1, length+1)]) + \
+                max([sum(nums[idx:length//2]) for idx in range(length//2-1, -1, -1)])
         return max(l_max, r_max, c_max)
